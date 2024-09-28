f7afdbd38193ef17f70de804d33a3665de3e7e34
Byron Johnson <bjohnson@nibzdil.org>
2024-09-28 10:39:43 -0600

    Add CLI Config.

---
+++ src/Immutaball/Ball/CLI/Config.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}  -- Support tab indentation better, for a better default of no warning if tabs are used: https://dmitryfrank.com/articles/indent_with_tabs_align_with_spaces .
+-- Enable warnings:
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+-- State.hs.
+
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Immutaball.Ball.CLI.Config
+	(
+		CLIConfig(..),
+		defaultCLIConfig
+	) where
+
+import Control.Lens
+
+data CLIConfig = CLIConfig {
+}
+	deriving (Eq, Ord)
+makeLenses ''CLIConfig
+
+defaultCLIConfig :: CLIConfig
+defaultCLIConfig = CLIConfig {
+}

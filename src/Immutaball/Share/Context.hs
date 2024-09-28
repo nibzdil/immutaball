07ea0cc0e33fccbe84e6d0c39f89efab0f6b8a20
Byron Johnson <bjohnson@nibzdil.org>
2024-09-28 09:50:08 -0600

    Add Context module.

---
+++ src/Immutaball/Share/Context.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}  -- Support tab indentation better, for a better default of no warning if tabs are used: https://dmitryfrank.com/articles/indent_with_tabs_align_with_spaces .
+-- Enable warnings:
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+-- State.hs.
+
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Immutaball.Share.Context
+	(
+		Context(..)
+	) where
+
+import Control.Lens
+
+-- | An Immutaball context instance.
+data Context = Context {
+}
+
+makeLenses ''Context

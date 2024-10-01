3ce9e202d0b2feb0f7f5e0ca1c46fe9e1c0793d5
Byron Johnson <bjohnson@nibzdil.org>
2024-09-30 21:07:38 -0600

    Add SDLManager module.

---
+++ src/Immutaball/Share/SDLManager.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}  -- Support tab indentation better, for a better default of no warning if tabs are used: https://dmitryfrank.com/articles/indent_with_tabs_align_with_spaces .
+-- Enable warnings:
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+-- State.hs.
+
+{-# LANGUAGE Haskell2010 #-}
+
+-- | Some SDL functions that some functions can only be called in the OS thread
+-- that set up video.
+--
+-- So I created a module that video setup and input event callers can route
+-- their requests through, to ensure they are called from the same OS thread to
+-- meet this requirement mentioned in the documentation.
+--
+-- Example:
+-- > pollEvent :: MonadIO m => m (Maybe Event)
+-- >
+-- > Poll for currently pending events. You can only call this function in the OS thread that set the video mode.
+module Immutaball.Share.SDLManager
+	(
+	) where
+
+import Prelude ()
+--import Immutaball.Prelude
+
+

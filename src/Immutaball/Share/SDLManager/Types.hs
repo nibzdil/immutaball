2da65dfcfca05e6d33ea69fc3e19a6d34ee3d5a2
Byron Johnson <bjohnson@nibzdil.org>
2024-10-03 12:11:47 -0600

    …Forgot to git add SDLManager.Types.
    Add.

---
+++ src/Immutaball/Share/SDLManager/Types.hs
@@ -0,0 +1,44 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}  -- Support tab indentation better, for a better default of no warning if tabs are used: https://dmitryfrank.com/articles/indent_with_tabs_align_with_spaces .
+-- Enable warnings:
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+-- State.hs.
+
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | Moved into another module to avoid Template Haskell errors.
+module Immutaball.Share.SDLManager.Types
+	(
+		SDLManagerHandle(..), sdlmh_done, sdlmh_doneReceived, sdlmh_commands,
+		SDLManagerCommand(..),
+	) where
+
+import Prelude ()
+import Immutaball.Prelude
+
+import Control.Concurrent.STM.TMVar
+import Control.Concurrent.STM.TVar
+import Control.Concurrent.STM.TChan
+import Control.Lens
+import SDL.Event
+
+-- Moved from later to fix Template Haskell errors.
+data SDLManagerCommand =
+	-- | Close the manager; it's done.
+	  QuitSDLManager
+	| NopSDLManager
+	-- | SDL.Event.pollEvent documents that it requires it be run in the OS
+	-- thread which set the video mode, which is why SDLManager solely manages
+	-- this.
+	| PollEvent (TMVar (Maybe Event))
+	deriving (Eq)
+
+-- | The inner fields and lenses are internal (low-level).
+data SDLManagerHandle = SDLManagerHandle {
+	_sdlmh_done :: TVar Bool,
+	_sdlmh_doneReceived :: TVar Bool,
+	_sdlmh_commands :: TChan SDLManagerCommand
+}
+	deriving (Eq)
+makeLenses ''SDLManagerHandle  -- Error for future module reference.

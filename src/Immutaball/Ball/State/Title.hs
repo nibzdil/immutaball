6ac3e633cfd441f12abaeab203e70bd533914775
Byron Johnson <bjohnson@nibzdil.org>
2024-10-04 22:51:03 -0600

    WIP: snapshot debugging.

--- src/Immutaball/Ball/State/Title.hs
+++ src/Immutaball/Ball/State/Title.hs
@@ -22,11 +22,19 @@ import Immutaball.Share.Context
 import Immutaball.Share.State
 import Immutaball.Share.State.Context
 import Immutaball.Share.Wire
+import Immutaball.Share.Utils
+
+import Debug.Trace  ---------------------------- TODO--
+import Immutaball.Share.ImmutaballIO
+import Immutaball.Share.ImmutaballIO.BasicIO
+import Immutaball.Share.ImmutaballIO.SDLIO
 
 -- TODO: FIXME: I see no window :(.
 -- TODO:
 mkTitleState :: IBContext -> Immutaball
-mkTitleState baseCxt0 = fromImmutaballSingle $ proc _request -> do
+--mkTitleState baseCxt0 = fromImmutaballSingle $ proc _request -> do
+{-
+mkTitleState baseCxt0 = trace "DEBUG1: start" . fromImmutaballSingle $ proc _request -> do
 	-- TODO: FIXME: GHC can't see initialCxt; fails with ‘not in scope’.
 	--let initialCxt = initialStateCxt baseCxt0
 	rec
@@ -37,5 +45,39 @@ mkTitleState baseCxt0 = fromImmutaballSingle $ proc _request -> do
 		cxtnp1 <- delayWire (initialStateCxt baseCxt0) requireVideo -< cxtn
 	-- TODO:
 	-- case request of  -- TODO
-	returnA -< Identity ContinueResponse
+	--returnA -< Identity ContinueResponse
+	returnA -< trace "DEBUG0: ContinueResponse" $ Identity ContinueResponse
 	--returnA -< Identity DoneResponse
+-}
+{-
+mkTitleState baseCxt0 = trace "DEBUG1: start" $ proc _request -> do
+	--returnA -< [DoneResponse]
+	--_ <- returnA -< ()
+	-- TODO FIXME: how come only the first IO is executed (without the
+	-- controller getting to the point of checking for allowWireForks), and
+	-- then it quits?
+	_ <- monadic -< liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG12: IO works!"
+	_ <- monadic -< liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG13: IO works!"
+	_ <- monadic -< liftIBIO . BasicImmutaballIOF . DelayUs $ (5 * 1000 * 1000)
+
+	--returnA -< [DoneResponse]
+	returnA -< [ContinueResponse]
+-}
+
+{-
+-- TODO: try a non-arrow notation version to see if it works.  Maybe (probably
+-- not) it's an Arrows syntax bug.  Maybe when composing it manually, you find
+-- something that doesn't make sense or think of something to check.
+mkTitleState baseCxt0 = trace "DEBUG1: start" $ -- . wire $ \_request ->
+	pure (liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG12: IO works!") >>> monadic >>>
+	pure (liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG13: IO works!") >>> monadic >>>
+	pure (liftIBIO . BasicImmutaballIOF . DelayUs $ (5 * 1000 * 1000)) >>> monadic >>>
+	pure [ContinueResponse]
+-}
+-- Okay, same results.  So it's not Arrow notation.
+-- Here, I'll just keep it here for now to reduce possible variables:
+mkTitleState baseCxt0 = trace "DEBUG1: start" $ -- . wire $ \_request ->
+	pure (liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG12: IO works!") >>> monadic >>>
+	pure (liftIBIO . BasicImmutaballIOF . PutStrLn $ "DEBUG13: IO works!") >>> monadic >>>
+	pure (liftIBIO . BasicImmutaballIOF . DelayUs $ (5 * 1000 * 1000)) >>> monadic >>>
+	pure [ContinueResponse]

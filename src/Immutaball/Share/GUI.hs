2750db049ada0392c44999f35513a4cf995acaae
Byron Johnson <bjohnson@nibzdil.org>
2024-10-08 15:42:12 -0600

    Use lens fields instead of manually.  Also widget prisms.

--- src/Immutaball/Share/GUI.hs
+++ src/Immutaball/Share/GUI.hs
@@ -10,15 +10,17 @@
 module Immutaball.Share.GUI
 	(
 		-- * widgets
-		Root(..), rootId,
-		Space(..), spaceId, spaceParent, spaceFill,
-		VStack(..), vstackId, vstackParent,
-		HStack(..), hstackId, hstackParent,
-		Label(..), labelId, labelParent, labelText,
-		Button(..), buttonId, buttonParent, buttonText,
-		Widget(..),
-		WidgetId(..),
-		WidgetParent(..),
+		HasWid(..),
+		HasWparent(..),
+		HasFill(..),
+		HasText(..),
+		Root(..), rootWid,
+		Space(..), spaceWid, spaceWparent, spaceFill,
+		Vstack(..), vstackWid, vstackWparent,
+		Hstack(..), hstackWid, hstackWparent,
+		Label(..), labelWid, labelWparent, labelText,
+		Button(..), buttonWid, buttonWparent, buttonText,
+		Widget(..), AsWidget(..),
 
 		-- * wires
 		WidgetRequest(..),
@@ -41,97 +43,87 @@ import Immutaball.Share.Wire
 -- * widgets
 
 data Root id = Root {
-	_rootId :: id
+	_rootWid :: id
 }
 	deriving (Eq, Ord, Show)
 makeLenses ''Root
+makeFields ''Root
 
 data Space id = Space {
-	_spaceId     :: id,
-	_spaceParent :: id,
+	_spaceWid     :: id,
+	_spaceWparent :: id,
 
 	-- | Max space to anchor at bottom.
-	_spaceFill   :: Bool
+	_spaceFill    :: Bool
 }
 	deriving (Eq, Ord, Show)
 makeLenses ''Space
+makeFields ''Space
 
-data VStack id = VStack {
-	_vstackId     :: id,
-	_vstackParent :: id
+instance HasWparent (Root id) id where wparent = wid
+
+data Vstack id = Vstack {
+	_vstackWid     :: id,
+	_vstackWparent :: id
 }
 	deriving (Eq, Ord, Show)
-makeLenses ''VStack
+makeLenses ''Vstack
+makeFields ''Vstack
 
-data HStack id = HStack {
-	_hstackId     :: id,
-	_hstackParent :: id
+data Hstack id = Hstack {
+	_hstackWid     :: id,
+	_hstackWparent :: id
 }
 	deriving (Eq, Ord, Show)
-makeLenses ''HStack
+makeLenses ''Hstack
+makeFields ''Hstack
 
 data Label id = Label {
-	_labelId     :: id,
-	_labelParent :: id,
+	_labelWid     :: id,
+	_labelWparent :: id,
 
-	_labelText   :: String
+	_labelText    :: String
 }
 	deriving (Eq, Ord, Show)
 makeLenses ''Label
+makeFields ''Label
 
 data Button id = Button {
-	_buttonId     :: id,
-	_buttonParent :: id,
+	_buttonWid     :: id,
+	_buttonWparent :: id,
 
-	_buttonText   :: String
+	_buttonText    :: String
 }
 	deriving (Eq, Ord, Show)
 makeLenses ''Button
+makeFields ''Button
 
 data Widget id =
 	  RootWidget   (Root   id)
 	| SpaceWidget  (Space  id)
-	| VStackWidget (VStack id)
-	| HStackWidget (HStack id)
+	| VstackWidget (Vstack id)
+	| HstackWidget (Hstack id)
 	| LabelWidget  (Label  id)
 	| ButtonWidget (Button id)
 	deriving (Eq, Ord, Show)
+makeClassyPrisms ''Widget
 
-class WidgetId w id | w -> id where
-	widgetId :: Lens' w id
-
-class WidgetParent w id | w -> id where
-	widgetParent :: Lens' w id
-
-instance WidgetId     (Root   id) id where widgetId     = rootId
-instance WidgetParent (Root   id) id where widgetParent = rootId
-instance WidgetId     (Space  id) id where widgetId     = spaceId
-instance WidgetParent (Space  id) id where widgetParent = spaceParent
-instance WidgetId     (VStack id) id where widgetId     = vstackId
-instance WidgetParent (VStack id) id where widgetParent = vstackParent
-instance WidgetId     (HStack id) id where widgetId     = hstackId
-instance WidgetParent (HStack id) id where widgetParent = hstackParent
-instance WidgetId     (Label  id) id where widgetId     = labelId
-instance WidgetParent (Label  id) id where widgetParent = labelParent
-instance WidgetId     (Button id) id where widgetId     = buttonId
-instance WidgetParent (Button id) id where widgetParent = buttonParent
-
-instance WidgetId (Widget id) id where
-	widgetId :: forall f. Functor f => (id -> f id) -> Widget id -> f (Widget id)
-	widgetId accessor (RootWidget   root)   = RootWidget   <$> widgetId accessor root
-	widgetId accessor (SpaceWidget  space)  = SpaceWidget  <$> widgetId accessor space
-	widgetId accessor (VStackWidget vstack) = VStackWidget <$> widgetId accessor vstack
-	widgetId accessor (HStackWidget hstack) = HStackWidget <$> widgetId accessor hstack
-	widgetId accessor (LabelWidget  label)  = LabelWidget  <$> widgetId accessor label
-	widgetId accessor (ButtonWidget button) = ButtonWidget <$> widgetId accessor button
-instance WidgetParent (Widget id) id where
-	widgetParent :: forall f. Functor f => (id -> f id) -> Widget id -> f (Widget id)
-	widgetParent accessor (RootWidget   root)   = RootWidget   <$> widgetParent accessor root
-	widgetParent accessor (SpaceWidget  space)  = SpaceWidget  <$> widgetParent accessor space
-	widgetParent accessor (VStackWidget vstack) = VStackWidget <$> widgetParent accessor vstack
-	widgetParent accessor (HStackWidget hstack) = HStackWidget <$> widgetParent accessor hstack
-	widgetParent accessor (LabelWidget  label)  = LabelWidget  <$> widgetParent accessor label
-	widgetParent accessor (ButtonWidget button) = ButtonWidget <$> widgetParent accessor button
+instance HasWid (Widget id) id where
+	wid :: forall f. Functor f => (id -> f id) -> Widget id -> f (Widget id)
+	wid accessor (RootWidget   root)   = RootWidget   <$> wid accessor root
+	wid accessor (SpaceWidget  space)  = SpaceWidget  <$> wid accessor space
+	wid accessor (VstackWidget vstack) = VstackWidget <$> wid accessor vstack
+	wid accessor (HstackWidget hstack) = HstackWidget <$> wid accessor hstack
+	wid accessor (LabelWidget  label)  = LabelWidget  <$> wid accessor label
+	wid accessor (ButtonWidget button) = ButtonWidget <$> wid accessor button
+instance HasWparent (Widget id) id where
+	wparent :: forall f. Functor f => (id -> f id) -> Widget id -> f (Widget id)
+	wparent accessor (RootWidget   root)   = RootWidget   <$> wparent accessor root
+	wparent accessor (SpaceWidget  space)  = SpaceWidget  <$> wparent accessor space
+	wparent accessor (VstackWidget vstack) = VstackWidget <$> wparent accessor vstack
+	wparent accessor (HstackWidget hstack) = HstackWidget <$> wparent accessor hstack
+	wparent accessor (LabelWidget  label)  = LabelWidget  <$> wparent accessor label
+	wparent accessor (ButtonWidget button) = ButtonWidget <$> wparent accessor button
 
 -- * wires
 
@@ -149,7 +141,7 @@ data WidgetResponse id =
 
 -- TODO:
 mkGUI :: [Widget id] -> Wire ImmutaballM (WidgetRequest id) (WidgetResponse id)
-mkGUI initialWidgets = proc request -> do
+mkGUI _initialWidgets = proc _request -> do
 	--widgets <- hold initialWidgets -< case request of ResetGUI widgets -> Just widgets_; _ -> Nothing
 	-- TODO:
 	returnA -< NoWidgetAction

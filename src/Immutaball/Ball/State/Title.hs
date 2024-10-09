381baa5c7f1c5cfef606a615e48046f0f25ccf6c
Byron Johnson <bjohnson@nibzdil.org>
2024-10-08 21:37:49 -0600

    The title state has a basic GUI.
    It's not rendered yet, but it can sure quit!  Yay!
    
    (I'll need to fix the initial focus, though).

--- src/Immutaball/Ball/State/Title.hs
+++ src/Immutaball/Ball/State/Title.hs
@@ -9,7 +9,9 @@
 
 module Immutaball.Ball.State.Title
 	(
-		mkTitleState
+		mkTitleState,
+		TitleWidget(..),
+		titleGui
 	) where
 
 import Prelude ()
@@ -18,15 +20,39 @@ import Immutaball.Prelude
 import Control.Arrow
 import Data.Functor.Identity
 
+import Immutaball.Share.GUI
 import Immutaball.Share.State
 import Immutaball.Share.State.Context
 import Immutaball.Share.Wire
 
--- TODO:
+-- TODO: go to next play state
 mkTitleState :: Either IBContext IBStateContext -> Immutaball
-mkTitleState baseCxt0 = fromImmutaballSingle $ proc (Identity _request) -> do
+mkTitleState baseCxt0 = fromImmutaballSingle $ proc (Identity request) -> do
 	rec
 		cxtn <- stateContextStorage cxt0 <<< Just <$> requireVideo <<< delay cxt0 -< cxtn
-	-- TODO
-	returnA -< pure ContinueResponse
+	guiResponse <- mkGUI titleGui -< GUIDrive request
+	response <- returnA -< case guiResponse of
+		NoWidgetAction          -> ContinueResponse
+		WidgetAction QuitButton -> DoneResponse
+		_                       -> ContinueResponse
+	returnA -< Identity response
 	where cxt0 = either initialStateCxt id baseCxt0
+
+data TitleWidget =
+	  TitleRoot
+	| MenuVstack
+	| PlayButton
+	| QuitButton
+	| Anonymous Integer
+	deriving (Eq, Ord, Show)
+
+titleGui :: [Widget TitleWidget]
+titleGui =
+	[
+		RootWidget   $ Root   { _rootWid   = TitleRoot                               },
+		VstackWidget $ Vstack { _vstackWid = MenuVstack, _vstackWparent = TitleRoot  },
+		ButtonWidget $ Button { _buttonWid = PlayButton, _buttonWparent = MenuVstack,
+			_buttonText = "Play" },
+		ButtonWidget $ Button { _buttonWid = QuitButton, _buttonWparent = MenuVstack,
+			_buttonText = "Quit" }
+	]

b96a1cce450b307e535815b31a59d1f49a5ef621
Byron Johnson <bjohnson@nibzdil.org>
2024-10-11 17:24:23 -0600

    Add .Video module.

---
+++ src/Immutaball/Share/Video.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}  -- Support tab indentation better, for a better default of no warning if tabs are used: https://dmitryfrank.com/articles/indent_with_tabs_align_with_spaces .
+-- Enable warnings:
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+-- CLI.hs.
+
+{-# LANGUAGE Haskell2010 #-}
+
+module Immutaball.Share.Video
+	(
+	) where
+
+import Prelude ()
+--import Immutaball.Prelude

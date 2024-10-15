9a8edf301a7de82682ebeeb14d5765ccb6a8cd76
Byron Johnson <bjohnson@nibzdil.org>
2024-10-15 16:55:37 -0600

    Fix Tex layer offset.

--- src/Immutaball/Share/GUI.hs
+++ src/Immutaball/Share/GUI.hs
@@ -407,7 +407,6 @@ guiPaintWidgets = proc (paintWidgets, _widgetLastFocus, _widgetIdx, _t, cxtn) ->
 	let verticesPerWidget = 2 * trianglesPerWidget
 	let numVertices = numWidgets * verticesPerWidget
 	let (vertexArray  :: UArray Integer GLdouble) = runSTUArray $ do
-		--array_ <- newGenArray (0, numWidgets - 1) $ \idx ->
 		array_ <- newArray (0, numVertices * vertexStrideByDoubles - 1) 0.0
 		when (numWidgets > 0) $ do
 			arrayAsGLuint <- castSTUArray' array_
@@ -437,7 +436,7 @@ guiPaintWidgets = proc (paintWidgets, _widgetLastFocus, _widgetIdx, _t, cxtn) ->
 					writeArray array_ (vidx*10 + 7) $ vt^.x2
 					writeArray array_ (vidx*10 + 8) $ vt^.y2
 					-- Tex layer (double indices since from this view elements are half as long).
-					writeArray arrayAsGLuint (2*(vidx*10 + 8) + 1) $ texture
+					writeArray arrayAsGLuint (2*(vidx*10 + 9) + 0) $ texture
 
 				withVertex vp1 vt1 (verticesPerWidget * idx + 0)
 				withVertex vp2 vt2 (verticesPerWidget * idx + 1)

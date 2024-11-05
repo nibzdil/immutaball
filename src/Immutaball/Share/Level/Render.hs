2f480e723e7257d5b385c960d07adeec937ebee8
Byron Johnson <bjohnson@nibzdil.org>
2024-11-05 14:12:48 -0700

    Add level render module.

--- src/Immutaball/Share/Level/Render.hs
+++ src/Immutaball/Share/Level/Render.hs
@@ -2,22 +2,13 @@
 -- Enable warnings:
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
--- Level.hs.
+-- Level/Render.hs.
 
 {-# LANGUAGE Haskell2010 #-}
 
-module Immutaball.Share.Level
+module Immutaball.Share.Level.Render
 	(
-		module Immutaball.Share.Level.Base,
-		module Immutaball.Share.Level.Render,
-		module Immutaball.Share.Level.Parser,
-		module Immutaball.Share.Level.Utils
 	) where
 
 import Prelude ()
 --import Immutaball.Prelude
-
-import Immutaball.Share.Level.Base
-import Immutaball.Share.Level.Render
-import Immutaball.Share.Level.Parser
-import Immutaball.Share.Level.Utils

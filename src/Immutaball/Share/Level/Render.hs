360b01cee27e650c84f25f0b313b7b5cf105e1ff
Byron Johnson <bjohnson@nibzdil.org>
2024-11-09 12:38:28 -0700

    Delegate to set up on new level for render.

--- src/Immutaball/Share/Level/Render.hs
+++ src/Immutaball/Share/Level/Render.hs
@@ -23,11 +23,16 @@ import Immutaball.Share.Level.Base
 import Immutaball.Share.Math
 import Immutaball.Share.State
 import Immutaball.Share.State.Context
+import Immutaball.Share.Utils
 import Immutaball.Share.Wire
 
 -- | TODO: implement.
 renderLevel :: Wire ImmutaballM ((MView, SolWithAnalysis), IBStateContext) IBStateContext
-renderLevel = proc ((_camera, swa), cxt0) -> do
+renderLevel = proc ((_camera, swa), cxtn) -> do
+	let levelPath = swa^.swaMeta.smPath
+	(mlastLevelPath, cxtnp1) <- setCurrentlyLoadedSOL -< (levelPath, cxtn)
+	let newLevel = Just levelPath == mlastLevelPath
+	cxtnp2 <- returnA ||| renderSetupNewLevel -< if' (not newLevel) (Left cxtnp1) (Right (swa, cxtnp1))
 	let
 		sol :: Sol
 		sol = swa^.swaSol
@@ -35,4 +40,8 @@ renderLevel = proc ((_camera, swa), cxt0) -> do
 		sa :: SolAnalysis
 		sa = swa^.swaSa
 	let _unused = (sol, sa)  -- TODO
-	returnA -< cxt0
+	returnA -< cxtnp2
+
+renderSetupNewLevel :: Wire ImmutaballM (SolWithAnalysis, IBStateContext) IBStateContext
+renderSetupNewLevel = proc (_swa, cxtn) -> do
+	returnA -< cxtn

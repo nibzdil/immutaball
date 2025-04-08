c14ea5c2bd0a098c13830f57cb1ee9c46b3ef76a
Byron Johnson <bjohnson@nibzdil.org>
2025-04-07 19:10:49 -0600

    Enable another debug.

--- src/Immutaball/Ball/State/Game.hs
+++ src/Immutaball/Ball/State/Game.hs
@@ -680,7 +680,7 @@ physicsBallAdvanceBruteForceCompute numCollisions thresholdTimeRemaining lastLi 
 								-- Make sure the point is behind this plane.
 								return $ plane3PointDistance sidejPlane planeIntersection <= 0
 					--D.trace "DEBUG5 (pass!)" $ return ()
-					--D.trace (printf "DEBUG5 (pass!); sidePlanes: %s" (show sidePlanes)) $ return ()
+					D.trace (printf "DEBUG5 (pass!); sidePlanes: %s" (show sidePlanes)) $ return ()
 					let debugPlanes = do
 						sj <- [lump^.lumpS0 .. lump^.lumpS0 + lump^.lumpSc - 1]
 						let sidej = (level^.solSv) ! sj

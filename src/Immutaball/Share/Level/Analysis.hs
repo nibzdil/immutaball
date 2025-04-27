186a607af9e0c0c70a49806081d05b429b10e710
Byron Johnson <bjohnson@nibzdil.org>
2025-04-26 18:47:26 -0600

    Remove most of the extra debugging now that it's been fixed.

--- src/Immutaball/Share/Level/Analysis.hs
+++ src/Immutaball/Share/Level/Analysis.hs
@@ -716,29 +716,10 @@ mkSolPhysicsAnalysis _cxt sol = (\r -> D.trace (printf "DEBUG1: bsp trees!: %s" 
 					-- our plane, find the closest lump to the mean and use its
 					-- mean.
 					let allMean' = closestLumpMean allLumps allMean in
-					--undefined $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
-					--normalizePlane3 (Vec3 0 0 0) (refineNormal allLumps allMean' initialNormal),  -- More progress
-					--normalizePlane3 (allMean) (refineNormal allLumps allMean' initialNormal),  -- Freezes early; only 1 fork debug print?
-					--normalizePlane3 (lumpsAverageVertex (S.fromList bodyLumpIndices)) (refineNormal allLumps allMean' initialNormal),  -- Freezes early; only 1 fork debug print?
-					--normalizePlane3 (D.trace (printf "DEBUG88: %s" (show $ (S.fromList bodyLumpIndices))) $ lumpsAverageVertex (S.fromList bodyLumpIndices)) (refineNormal allLumps allMean' initialNormal),  -- Freezes early; only 1 fork debug print?
-					--normalizePlane3 (D.trace (printf "DEBUG88: %s" (show $ ())) $ lumpsAverageVertex (S.fromList bodyLumpIndices)) (refineNormal allLumps allMean' initialNormal),  -- Freezes early; only 1 fork debug print?
-					--D.trace (printf "DEBUG89: %s" (show $ (lumpsAverageVertex (S.fromList bodyLumpIndices), (refineNormal allLumps allMean' initialNormal)))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- debug does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show 3)) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- debug prints!!!  Something in the debug is freezing it.
-					--D.trace (printf "DEBUG89: %s" (show (refineNormal allLumps allMean' initialNormal))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (allLumps, allMean', initialNormal))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (allLumps, allMean'))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (allLumps))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- prints.
-					--D.trace (printf "DEBUG89: %s" (show (allMean'))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (allMean))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (lumpsAverageVertex (S.fromList bodyLumpIndices)))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.
-					--D.trace (printf "DEBUG89: %s" (show (S.fromList bodyLumpIndices))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- prints.  Hmm, how come adding lumpsAverageVertex adds a freeze?
-					--D.trace (printf "DEBUG89: %s" (show (S.fromList bodyLumpIndices))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
-					--D.trace (printf "DEBUG89: %s" (show (S.map (\li -> flip M.lookup (spa^.spaLumpAverageVertex) li) $ S.fromList bodyLumpIndices))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal), -- Prints.
-					--D.trace (printf "DEBUG89: %s" (show (S.map (\x -> x `morElse` error "Internal error: mkSolPhysicsAnalysis lumpsAverageVertex failed to find average vertex for a lump") . S.map (\li -> flip M.lookup (spa^.spaLumpAverageVertex) li) $ S.fromList bodyLumpIndices))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- prints.
-					D.trace (printf "DEBUG89: %s" (show (steppingMean . S.map (\x -> x `morElse` error "Internal error: mkSolPhysicsAnalysis lumpsAverageVertex failed to find average vertex for a lump") . S.map (\li -> flip M.lookup (spa^.spaLumpAverageVertex) li) $ S.fromList bodyLumpIndices))) $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),  -- does NOT print.  Narrowed down to steppingMean.
-				_lbsppLumps = undefined $ S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
+					normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
+				_lbsppLumps = S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
 
-				_lbsppLumpsMeanVertex = undefined $ lumpsAverageVertex (partition^.lbsppLumps),
+				_lbsppLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppLumps),
 
 				_lbsppAllLumps = S.fromList bodyLumpIndices,
 				_lbsppAllLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppAllLumps)
@@ -759,14 +740,13 @@ mkSolPhysicsAnalysis _cxt sol = (\r -> D.trace (printf "DEBUG1: bsp trees!: %s" 
 						-- our plane, find the closest lump to the mean and use its
 						-- mean.
 						let allMean' = closestLumpMean allLumps allMean in
-						undefined $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
-					_lbsppLumps = undefined $ S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
+						normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
+					_lbsppLumps = S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
 
-					_lbsppLumpsMeanVertex = undefined $ lumpsAverageVertex (partition^.lbsppLumps),
+					_lbsppLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppLumps),
 
-					--_lbsppAllLumps = S.filter (\li -> lumpPlaneSide (parentPartition^.lbsppPlane) li == (-1)) (parentPartition^.lbsppAllLumps),
-					_lbsppAllLumps = S.filter (\li -> D.trace "DEBUG10allLumps" $ lumpPlaneSide (parentPartition^.lbsppPlane) li == (-1)) (parentPartition^.lbsppAllLumps),
-					_lbsppAllLumpsMeanVertex = undefined $ lumpsAverageVertex (partition^.lbsppAllLumps)
+					_lbsppAllLumps = S.filter (\li -> lumpPlaneSide (parentPartition^.lbsppPlane) li == (-1)) (parentPartition^.lbsppAllLumps),
+					_lbsppAllLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppAllLumps)
 				})
 				(parentPartition & lbsppLumps %~ assertNonNull)
 				(makeTree . fix $ \partition -> LumpBSPPartition {
@@ -779,14 +759,13 @@ mkSolPhysicsAnalysis _cxt sol = (\r -> D.trace (printf "DEBUG1: bsp trees!: %s" 
 						-- our plane, find the closest lump to the mean and use its
 						-- mean.
 						let allMean' = closestLumpMean allLumps allMean in
-						undefined $ normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
-					_lbsppLumps = undefined $ S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
+						normalizePlane3 allMean' (refineNormal allLumps allMean' initialNormal),
+					_lbsppLumps = S.filter (lumpIntersectsPlane (partition^.lbsppPlane)) (partition^.lbsppAllLumps),
 
-					_lbsppLumpsMeanVertex = undefined $ lumpsAverageVertex (partition^.lbsppLumps),
+					_lbsppLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppLumps),
 
-					--_lbsppAllLumps = S.filter (\li -> lumpPlaneSide (parentPartition^.lbsppPlane) li == 1) (parentPartition^.lbsppAllLumps),
-					_lbsppAllLumps = S.filter (\li -> D.trace "DEBUG9allLumps" $ lumpPlaneSide (parentPartition^.lbsppPlane) li == 1) (parentPartition^.lbsppAllLumps),
-					_lbsppAllLumpsMeanVertex = undefined $ lumpsAverageVertex (partition^.lbsppAllLumps)
+					_lbsppAllLumps = S.filter (\li -> lumpPlaneSide (parentPartition^.lbsppPlane) li == 1) (parentPartition^.lbsppAllLumps),
+					_lbsppAllLumpsMeanVertex = lumpsAverageVertex (partition^.lbsppAllLumps)
 				})
 
 			where

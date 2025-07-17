0684dfa797b8e86828d30ae3c50c68aa95c502db
Byron Johnson <bjohnson@nibzdil.org>
2025-07-16 19:19:14 -0600

    Fallback Wire: reduce laziness.
    In an effort to improve performance of the fallback Wire implementation, reduce
    laziness.

--- src/dependency-substitutions/wires/Control/Wire/Internal.hs
+++ src/dependency-substitutions/wires/Control/Wire/Internal.hs
@@ -28,25 +28,25 @@ stepWire :: Wire m a b -> a -> m (b, Wire m a b)
 stepWire = _wireStep
 
 instance (Functor m) => Functor (Wire m a) where
-	fmap f ~(Wire w) = Wire $ \a -> (\ ~(b, w') -> (f b, fmap f w')) <$> w a
+	fmap f (Wire w) = Wire $ \a -> (\(b, w') -> (f b, fmap f w')) <$> w a
 
 instance (Applicative m) => Applicative (Wire m i) where
 	pure a = fix $ \me -> Wire $ \_i -> pure (a, me)
-	~(Wire wf) <*> ~(Wire wa) = Wire $ \i -> pure (\ ~(f, wf') ~(a, wa') -> (f a, wf' <*> wa')) <*> wf i <*> wa i
+	(Wire wf) <*> (Wire wa) = Wire $ \i -> pure (\(f, wf') (a, wa') -> (f a, wf' <*> wa')) <*> wf i <*> wa i
 
 instance (Monad m) => Category (Wire m) where
 	id = fix $ \me -> Wire $ \a -> return (a, me)
-	~(Wire wc) . ~(Wire wb) = Wire $ \a -> wb a >>= \ ~(b, wb') -> wc b >>= \ ~(c, wc') -> return (c, wc' C.. wb')
+	(Wire wc) . (Wire wb) = Wire $ \a -> wb a >>= \(b, wb') -> wc b >>= \(c, wc') -> return (c, wc' C.. wb')
 
 instance (Monad m) => Arrow (Wire m) where
 	arr f = fix $ \me -> Wire $ \a -> pure (f a, me)
-	first ~(Wire w) = Wire $ \ ~(a, d) -> (\ ~(b, w') -> ((b, d), first w')) <$> w a
+	first (Wire w) = Wire $ \(a, d) -> (\(b, w') -> ((b, d), first w')) <$> w a
 
 instance (Monad m) => ArrowChoice (Wire m) where
 	-- (Tricky: in the Left case, don't use ‘me’ which would discard ‘w'’.)
-	left ~(Wire w) = fix $ \me -> Wire $ \bd -> either (\b -> (\ ~(c, w') -> (Left c, left w')) <$> w b) (\d -> pure (Right d, me)) bd
+	left (Wire w) = fix $ \me -> Wire $ \bd -> either (\b -> (\(c, w') -> (Left c, left w')) <$> w b) (\d -> pure (Right d, me)) bd
 
 instance (MonadFix m) => ArrowLoop (Wire m) where
 	-- w :: (b, d) -> m ((c, d), Wire m (b, d) (c, d))
 	--  Make sure this is lazy enough, or it could hang!
-	loop ~(Wire w) = Wire $ \b -> (\ ~(~(c, _d), w') -> (c, loop w')) <$> (mfix $ \ ~(~(_c, d), _w') -> w (b, d))
+	loop (Wire w) = Wire $ \b -> (\((c, _d), w') -> (c, loop w')) <$> (mfix $ \ ~(~(_c, d), _w') -> w (b, d))
